ffca168a2e13202a9096ae45c4002ad30269fff8
ro-per <56132002+ro-per@users.noreply.github.com>
2021-03-15 16:00:17 +0100

    avoid middle

--- pylos-student/src/main/java/be/kuleuven/pylos/player/student/SearchFactory/SearchLocationA1.java
+++ pylos-student/src/main/java/be/kuleuven/pylos/player/student/SearchFactory/SearchLocationA1.java
@@ -5,6 +5,7 @@ import be.kuleuven.pylos.game.PylosLStructure;
 import be.kuleuven.pylos.game.PylosLocation;
 import be.kuleuven.pylos.player.PylosPlayer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchLocationA1 extends SearchLocation {
@@ -15,6 +16,27 @@ public class SearchLocationA1 extends SearchLocation {
     @Override
     public PylosLocation getLocation(PylosBoard board, PylosPlayer pp) {
         List<PylosLStructure> lStructuresOwnColor = getLStructureList(pp.PLAYER_COLOR, board);
-        return A1_getForthEmptyLocation(lStructuresOwnColor);
+        return A1_getForthEmptyLocation(board, lStructuresOwnColor);
+    }
+
+    private PylosLocation A1_getForthEmptyLocation(PylosBoard board, List<PylosLStructure> lStructuresOwnColor) {
+        List<PylosLocation> temp1 = new ArrayList<>();
+        if (!lStructuresOwnColor.isEmpty()) {
+            for (PylosLStructure pls : lStructuresOwnColor) {
+                if (pls.isForthLocationEmpty()) {
+                    temp1.add(pls.getPylosLocation4());
+                }
+            }
+        }
+        List<PylosLocation> temp2 = equalsMiddleLocations(board, temp1);
+        if (!temp2.isEmpty()) {
+            return temp2.get(0);
+        }
+        else if(!temp1.isEmpty()) {
+            return temp1.get(0);
+        }
+
+        return null;
+
     }
 }
